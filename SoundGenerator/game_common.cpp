11204f3fde1bf4fdcbe56656f2c7ff4402aabd8a
Eike Decker <zet23t@googlemail.com>
2018-01-19 22:28:16 +0100

    Updating project file and code files

--- SoundGenerator/game_common.cpp
+++ SoundGenerator/game_common.cpp
@@ -85,16 +85,51 @@ namespace Game {
             const uint8_t BUTTON_PLAY = 2;
             const uint8_t WINDOW_SAMPLES = 3;
             const uint8_t WINDOW_SAMPLES_ACTIVE = 4;
+
+            const uint8_t MENU_BUTTON_AMP_MORE = 20;
+            const uint8_t MENU_BUTTON_AMP_LESS = 21;
+            const uint8_t MENU_BUTTON_OK = 22;
+            const uint8_t MENU_BUTTON_CANCEL = 23;
+            const uint8_t MENU_BUTTON_FRQ_MORE = 24;
+            const uint8_t MENU_BUTTON_FRQ_LESS = 25;
+
+            const uint8_t CONTEXT_MENU_CLEAR = 10;
+            const uint8_t CONTEXT_MENU_NOISE_ADD = 11;
+            const uint8_t CONTEXT_MENU_COPY = 12;
+            const uint8_t CONTEXT_MENU_PASTE_SET = 13;
+            const uint8_t CONTEXT_MENU_PASTE_ADD = 14;
+            const uint8_t CONTEXT_MENU_SET_END = 15;
+            const uint8_t CONTEXT_MENU_CANCEL = 16;
         }
+        const char* CONTEXT_MENU_TEXT[] = {
+            "Clear",
+            "Add noise",
+            "Copy",
+            "Paste (set)",
+            "Paste (add)",
+            "Set end",
+            "Cancel",
+        };
         const uint8_t FOCUS_SEQUENCE[] = {
             Element::BUTTON_MENU,
             Element::BUTTON_PLAY,
             Element::WINDOW_SAMPLES,
+            Element::BUTTON_MENU,
+
+            Element::CONTEXT_MENU_CLEAR,
+            Element::CONTEXT_MENU_SET_END,
+            Element::CONTEXT_MENU_NOISE_ADD,
+            Element::CONTEXT_MENU_COPY,
+            Element::CONTEXT_MENU_PASTE_ADD,
+            Element::CONTEXT_MENU_PASTE_SET,
+            Element::CONTEXT_MENU_CANCEL,
+            Element::CONTEXT_MENU_CLEAR,
+
 
         };
         #define FOCUS_SEQUENCE_COUNT (sizeof(FOCUS_SEQUENCE))
         int8_t selectedElement = Element::BUTTON_MENU;
-        uint16_t cursorPos;
+        uint16_t cursorPos, cursorStartPos;
         uint32_t blockJoystick;
         uint8_t hasDragged;
 
@@ -110,22 +145,88 @@ namespace Game {
         void prevElement() {
             for (uint8_t i = 0; i < FOCUS_SEQUENCE_COUNT; i+=1) {
                 if (selectedElement == FOCUS_SEQUENCE[i]) {
+                    for (uint8_t j = i+1; j<FOCUS_SEQUENCE_COUNT; j+=1) {
+                        if (selectedElement == FOCUS_SEQUENCE[j]) {
+                            selectedElement = FOCUS_SEQUENCE[j-1];
+                            return;
+                        }
+                    }
                     selectedElement = FOCUS_SEQUENCE[(i-1+FOCUS_SEQUENCE_COUNT)%FOCUS_SEQUENCE_COUNT];
                     break;
                 }
             }
         }
+
+        void drawContextMenu(int8_t dx, int8_t dy) {
+            if (selectedElement < Element::CONTEXT_MENU_CLEAR || selectedElement > Element::CONTEXT_MENU_CANCEL) return;
+            buffer.setOffset(-48,0);
+            buffer.drawRect(0,0,48,64)->filledRect(RGB565(40,60,90));
+            int i = 0,y = 0;
+            buffer.drawRect(0,0,48,8)->filledRect(RGB565(80,100,140));
+            drawText("-Selection-",2,0,48-4,8);
+
+            while (i < FOCUS_SEQUENCE_COUNT && FOCUS_SEQUENCE[i] != Element::CONTEXT_MENU_CLEAR)i++;
+            while (i < FOCUS_SEQUENCE_COUNT && (y == 0 || FOCUS_SEQUENCE[i] != Element::CONTEXT_MENU_CLEAR)) {
+                uint8_t item = FOCUS_SEQUENCE[i];
+                const char *str = CONTEXT_MENU_TEXT[item-Element::CONTEXT_MENU_CLEAR];
+                if (selectedElement == item) buffer.drawRect(0,y+9,48,7)->filledRect(RGB565(60,80,100));
+                drawText(str,2,y+9,48-4,8);
+                y+=8;
+                i++;
+            }
+            buffer.setOffset(0,0);
+            if (isReleased(0)) {
+                uint16_t a = cursorPos < cursorStartPos ? cursorPos : cursorStartPos;
+                uint16_t b = cursorPos >= cursorStartPos ? cursorPos : cursorStartPos;
+
+                switch (selectedElement) {
+                case Element::CONTEXT_MENU_SET_END:
+                    sampleCount = cursorStartPos;
+                    break;
+                case Element::CONTEXT_MENU_CLEAR:
+                    for (int i=a;i<=b;i+=1) editSamples[i] = 0;
+                    break;
+                case Element::CONTEXT_MENU_NOISE_ADD:
+                    break;
+                }
+                selectedElement = Element::WINDOW_SAMPLES_ACTIVE;
+            }
+
+
+        }
+
+        void showContextMenu() {
+            selectedElement = Element::CONTEXT_MENU_CLEAR;
+        }
+
         void drawSamples(bool focused, int8_t dx, int8_t dy) {
-            buffer.drawRect(sampleCount,14,1,36)->filledRect(RGB565(35,64,128));
+            int16_t xoff = cursorPos - 48;
+            if (xoff < 0) xoff = 0;
+            if (xoff > sizeof(editSamples) - 96) xoff = sizeof(editSamples) - 96;
             drawText(stringBuffer.start()
                      .putDec(editSamples[cursorPos]).put(" @ ")
                      .putDec(cursorPos).put(':').putDec(cursorPos*1000/FREQUENCY).put("ms").get(),0,55,96,9);
+            buffer.setOffset(xoff,0);
+            buffer.drawRect(sampleCount,14,1,36)->filledRect(RGB565(35,64,128));
             if (selectedElement == Element::WINDOW_SAMPLES_ACTIVE) {
                 if (isReleased(0) && !hasDragged) selectedElement = Element::WINDOW_SAMPLES;
-                if (isReleased(1) && !hasDragged) sampleCount = cursorPos;
-                buffer.drawRect(0,16,96,32)->filledRect(RGB565(0,0,64));
+                if (isReleased(1) && !hasDragged) {
+                    //sampleCount = cursorPos;
+                    showContextMenu();
+                }
+                buffer.drawRect(0,16,sizeof(editSamples),32)->filledRect(RGB565(0,0,64));
                 cursorPos += isHeld(0) ? dx * 8 : dx;
                 cursorPos %= sizeof(editSamples);
+                if (!isHeld(1) && dx) {
+                    cursorStartPos = cursorPos;
+                }
+                if (cursorStartPos != cursorPos) {
+                    uint16_t a = cursorPos < cursorStartPos ? cursorPos : cursorStartPos;
+                    uint16_t b = cursorPos >= cursorStartPos ? cursorPos : cursorStartPos;
+                    int16_t c = b - a;
+
+                    buffer.drawRect(a,15,c,34)->filledRect(RGB565(32,64,128));
+                }
                 buffer.drawRect(cursorPos,15,1,34)->filledRect(RGB565(255,255,255));
                 editSamples[cursorPos] += isHeld(0) ? dy * 8 : dy;
             } else if (selectedElement == Element::WINDOW_SAMPLES) {
@@ -140,8 +241,8 @@ namespace Game {
                 if (s<0) y = 32 - h;
                 buffer.drawRect(i,y,1,h)->filledRect(RGB565(128,128,255));
             }
-            buffer.drawRect(0,32,96,1)->filledRect(!focused || Time::millis %256 > 128 ? RGB565(0,0,128) : RGB565(0,0,255));
-
+            buffer.drawRect(0,32,sizeof(editSamples),1)->filledRect(!focused || Time::millis %256 > 128 ? RGB565(0,0,128) : RGB565(0,0,255));
+            buffer.setOffset(0,0);
         }
         void drawUI() {
             Fixed2D4 stick = Joystick::getJoystick();
@@ -170,6 +271,8 @@ namespace Game {
             if (button(96-9,0,9,9,ImageAsset::TextureAtlas_atlas::play.sprites[0], selectedElement == Element::BUTTON_PLAY)) {
                 playEditSample(false);
             }
+
+            drawContextMenu(x,y);
             if (!wasHeld(0) && !wasHeld(1)) hasDragged = 0;
 
         }
